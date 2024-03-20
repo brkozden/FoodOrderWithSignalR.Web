46f85749402d67dbb1a5d988d86652bd395a30b0
Berk Özden <berkozden5@gmail.com>
2024-03-21 00:49:26 +0300

    The manager classes in the concrete folder in the business layer were created and their codes were written.

---
+++ FoodOrder.Business/Concrete/BookingManager.cs
@@ -0,0 +1,46 @@
+﻿using FoodOrder.Business.Abstrack;
+using FoodOrder.DataAccess.Abstrack;
+using FoodOrder.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Business.Concrete
+{
+    public class BookingManager : IBookingService
+    {
+        private readonly IBookingDal _bookingDal;
+
+        public BookingManager(IBookingDal bookingDal)
+        {
+            _bookingDal = bookingDal;
+        }
+
+        public void TAdd(Booking entity)
+        {
+            _bookingDal.Add(entity);
+        }
+
+        public void TDelete(Booking entity)
+        {
+            _bookingDal.Delete(entity);
+        }
+
+        public List<Booking> TGetAll()
+        {
+            return _bookingDal.GetAll();
+        }
+
+        public Booking TGetById(int id)
+        {
+           return _bookingDal.GetById(id);
+        }
+
+        public void TUpdate(Booking entity)
+        {
+            _bookingDal.Update(entity);
+        }
+    }
+}
---
+++ FoodOrder.Business/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using FoodOrder.Business.Abstrack;
+using FoodOrder.DataAccess.Abstrack;
+using FoodOrder.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Business.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void TAdd(Contact entity)
+        {
+            _contactDal.Add(entity);
+        }
+
+        public void TDelete(Contact entity)
+        {
+            _contactDal.Delete(entity);
+        }
+
+        public List<Contact> TGetAll()
+        {
+          return  _contactDal.GetAll();
+        }
+
+        public Contact TGetById(int id)
+        {
+           return _contactDal.GetById(id);
+        }
+
+        public void TUpdate(Contact entity)
+        {
+            _contactDal.Update(entity);
+        }
+    }
+}
