d4b8cb30e3529f73a28b9974c4f427212b570d0b
Berk Özden <berkozden5@gmail.com>
2024-03-30 01:46:00 +0300

    User profile information update was added to the project.

---
+++ FoodOrder.WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using FoodOrder.Entity.Entities;
+using FoodOrder.WebUI.Dtos.IdentityDtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrder.WebUI.Controllers
+{
+    
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditDto userEditDto = new UserEditDto()
+            {
+                Name = value.Name,
+                Surname = value.Surname,
+                Mail = value.Email,
+                Username = value.UserName,
+            };
+            return View(userEditDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+           if (userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);  
+                user.Name = userEditDto.Name;
+                user.Surname = userEditDto.Surname;
+                user.Email = userEditDto.Mail;
+                user.UserName = userEditDto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+                 await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index","Login");
+            }
+           return NoContent();
+        }
+    }
+}
